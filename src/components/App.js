8671ed35ecb30f4ee7ff6f9d4855f824164506a0
Laura-O <laura.ohrndorf@gmail.com>
2018-01-31 13:12:22 +0100

    Add ajax request to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import Logo from './components/Logo.js';
-import RegistrationForm from './components/Registration.js';
-import Welcome from './components/Welcome.js';
-import './css/index.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Logo from './Logo.js';
+import Registration from './Registration.js';
+import Welcome from './Welcome.js';
 
-const App = () => (
-    <div className="container">
-        <Logo />
-        <Welcome />
-        <RegistrationForm />
-    </div>,
-  )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        axios.get('/user').then(({ data }) => {
+            console.log(data);
+            this.setState({ first: data.first, last: data.last, id: data.id });
+            console.log(this.state);
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div>I'm the app component</div>
+                <Logo />
+                {/* <Welcome /> */}
+                {/* <Registration /> */}
+            </div>
+        );
+    }
+}
 
 export default App;
